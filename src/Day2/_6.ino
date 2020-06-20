4a13b7cf9874c25aea4e04fd8dbbb1a2eafa65ea
Sasha <lonagi22@gmail.com>
2020-06-20 20:09:17 +0300

    Pattern for Pulse Width Modulation

---
+++ src/Day2/_6.ino
@@ -0,0 +1,11 @@
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+  analogWrite(5, map(analogRead(A0), 0, 1023, 0, 255));
+  delay(10); // Delay a little bit to improve simulation performance
+}
