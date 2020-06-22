d013d47ea655762ba8aa1c00793bfa5ae87e7123
Sasha <lonagi22@gmail.com>
2020-06-22 21:51:03 +0300

    Update _7.ino

--- src/Day3/_7.ino
+++ src/Day3/_7.ino
@@ -1,21 +1,18 @@
 #include <Servo.h>
 
-
 Servo servo_8;
 
-void setup()
-{
-  pinMode(2, INPUT);
-  servo_8.attach(8);
-
+void setup(){
+	pinMode(2, INPUT);
+	servo_8.attach(8);
 }
 
-void loop()
-{
-  if (digitalRead(2) == HIGH) {
-    servo_8.write(90);
-    delay(5000); // Wait for 5000 millisecond(s)
-  } else {
-    servo_8.write(0);
-  }
+void loop(){
+	if (digitalRead(2) == HIGH)
+	{
+		servo_8.write(90);
+		delay(5000); // Wait for 5000 millisecond(s)
+	}
+	else
+		servo_8.write(0);
 }
