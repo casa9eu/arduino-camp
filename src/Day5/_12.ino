36319c0b52866ae9b2b732a7aef1681e21149a73
Sasha <lonagi22@gmail.com>
2020-06-23 18:15:32 +0300

    Update _12.ino

--- src/Day5/_12.ino
+++ src/Day5/_12.ino
@@ -1,5 +1,4 @@
-void setup()
-{
+void setup(){
   pinMode(A0, INPUT);
   Serial.begin(9600);
 
@@ -7,13 +6,13 @@ void setup()
   pinMode(0, OUTPUT);
 }
 
-void loop()
-{
+void loop(){
   Serial.println(map(analogRead(A0), 0, 1023, 0, 100));
-  if (map(analogRead(A0), 0, 1023, 0, 100) < 20) {
+  
+  if (map(analogRead(A0), 0, 1023, 0, 100) < 20)
     tone(9, 523, 10000); // play tone 60 (C5 = 523 Hz)
-  } else {
+  else
     noTone(0);
-  }
+
   delay(10); // Delay a little bit to improve simulation performance
 }
