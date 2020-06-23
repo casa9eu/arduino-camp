5fa74234181f2a97c449a3c9fbe6739f61fc7057
Sasha <lonagi22@gmail.com>
2020-06-23 18:15:25 +0300

    Update _8.ino

--- src/Day4/_8.ino
+++ src/Day4/_8.ino
@@ -1,18 +1,18 @@
-void setup()
-{
-  pinMode(A0, INPUT);
-  pinMode(8, OUTPUT);
-  Serial.begin(9600);
+const int MainValue = 950;
+const int Pin = 8;
 
+void setup(){
+  pinMode(A0, INPUT);
+  pinMode(Pin, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop()
-{
-  if (analogRead(A0) >= 950) {                               //редактировать здесь
-    digitalWrite(8, HIGH);
-  } else {
-    digitalWrite(8, LOW);
-  }
+void loop(){
+  if (analogRead(A0) >= MainValue)
+    digitalWrite(Pin, HIGH);
+  else
+    digitalWrite(Pin, LOW);
+
   Serial.println(analogRead(A0));
   delay(10); 
 }
