5cc5cb062d100690ab81047e005a1208e678e952
Sasha <lonagi22@gmail.com>
2020-06-23 18:15:27 +0300

    Update _9.ino

--- src/Day4/_9.ino
+++ src/Day4/_9.ino
@@ -1,18 +1,18 @@
-void setup()
-{
-  pinMode(A0, INPUT);
-  pinMode(8, OUTPUT);
-  Serial.begin(9600);
+const int MainValue = 950;
+const int Pin = 8;
 
+void setup(){
+  pinMode(A0, INPUT);
+  pinMode(Pin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  if (analogRead(A0) >= 950) {                              //редактировать здесь
-    tone(8, 1661, 100000); // play tone 80 (G#6 = 1661 Hz)
-  } else {
-    noTone(8);
-  }
+  if (analogRead(A0) >= MainValue)
+    tone(Pin, 1661, 100000); // play tone 80 (G#6 = 1661 Hz)
+  else
+    noTone(Pin);
   Serial.println(analogRead(A0));
   delay(10); 
 }
