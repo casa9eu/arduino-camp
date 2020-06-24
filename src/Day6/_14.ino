895afa4ffdcbbc1b9993c76cb3b92f6eb67217f1
Sasha <lonagi22@gmail.com>
2020-06-24 21:38:20 +0300

    Update _14.ino

--- src/Day6/_14.ino
+++ src/Day6/_14.ino
@@ -2,39 +2,43 @@
 #include <MFRC522.h> // библиотека "RFID".
 #define SS_PIN 10
 #define RST_PIN 9
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
+unsigned long uidDec, uidDecTemp;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Waiting for card...");
-  SPI.begin();  //  инициализация SPI / Init SPI bus.
-  mfrc522.PCD_Init();     // инициализация MFRC522 / Init MFRC522 card.
+  SPI.begin(); 
+  mfrc522.PCD_Init();
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
 }
+
 void loop() {
   // Поиск новой метки
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  if ( ! mfrc522.PICC_IsNewCardPresent())
     return;
-  }
+
   // Выбор метки
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
+  if ( ! mfrc522.PICC_ReadCardSerial())
     return;
-  }
+
   uidDec = 0;
-  // Выдача серийного номера метки.
   for (byte i = 0; i < mfrc522.uid.size; i++)
   {
     uidDecTemp = mfrc522.uid.uidByte[i];
     uidDec = uidDec * 256 + uidDecTemp;
   }
+
   Serial.println("Card UID: ");
-  Serial.println(uidDec); // Выводим UID метки в консоль.
-  if (uidDec == 3763966293) // Сравниваем Uid метки, если он равен заданому то серва открывает.
+  Serial.println(uidDec);
+  if (uidDec == 3763966293)
   {
     tone(6, 200, 500); // Делаем звуковой сигнал, Открытие
     digitalWrite(7, HIGH);
     delay(3000); // пауза 3 сек и механизм запирается.
   }
+
   digitalWrite(7, LOW);
 }
