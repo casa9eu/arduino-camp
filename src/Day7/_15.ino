c1ba8cbd8f8c344d56730da8f677e211885490f4
Sasha <lonagi22@gmail.com>
2020-06-24 21:34:28 +0300

    Create _15.ino

---
+++ src/Day7/_15.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo servo_0;
+
+Servo servo_1;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  servo_0.attach(0);
+
+  pinMode(A1, INPUT);
+  servo_1.attach(1);
+
+}
+
+void loop()
+{
+  servo_0.write(map(analogRead(A0), 0, 1023, 0, 180));
+  servo_1.write(map(analogRead(A1), 0, 1023, 0, 180));
+  delay(10); // Delay a little bit to improve simulation performance
+}
