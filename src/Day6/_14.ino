537b1a899b377d7e27b1e2b93564ad1ff1582058
Sasha <lonagi22@gmail.com>
2020-06-24 21:34:25 +0300

    Create _14.ino

---
+++ src/Day6/_14.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <MFRC522.h> // библиотека "RFID".
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Waiting for card...");
+  SPI.begin();  //  инициализация SPI / Init SPI bus.
+  mfrc522.PCD_Init();     // инициализация MFRC522 / Init MFRC522 card.
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+void loop() {
+  // Поиск новой метки
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  // Выбор метки
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  uidDec = 0;
+  // Выдача серийного номера метки.
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    uidDecTemp = mfrc522.uid.uidByte[i];
+    uidDec = uidDec * 256 + uidDecTemp;
+  }
+  Serial.println("Card UID: ");
+  Serial.println(uidDec); // Выводим UID метки в консоль.
+  if (uidDec == 3763966293) // Сравниваем Uid метки, если он равен заданому то серва открывает.
+  {
+    tone(6, 200, 500); // Делаем звуковой сигнал, Открытие
+    digitalWrite(7, HIGH);
+    delay(3000); // пауза 3 сек и механизм запирается.
+  }
+  digitalWrite(7, LOW);
+}
