b4552a256a6c0b495f27719c3ca05203cde980fe
Sasha <lonagi22@gmail.com>
2020-06-27 00:18:25 +0300

    Update _20.ino

--- src/Day9/_20.ino
+++ src/Day9/_20.ino
@@ -1,5 +1,3 @@
-
-
 #define SW 0
 #define DT 2
 #define CLK 3
@@ -9,19 +7,16 @@ int ton = 500;
 Encoder enc1(CLK, DT, SW);
 
 void setup() {
-  Serial.begin(9600);
-  enc.setType(TYPE1); //если не работает можно попробовать изменить на TYPE2
-  pinMode(11, OUTPUT);
-
+	Serial.begin(9600);
+	enc.setType(TYPE1); //если не работает можно попробовать изменить на TYPE2
+	pinMode(11, OUTPUT);
 }
 
 void loop() {
-  enc1.tick();    // оставляем тут для работы "временных" функций и антидребезга
-  
-  if (enc1.isRight())ton += 50;         // если был поворот
-  if (enc1.isLeft()) ton -= 50;
-  Serial.println(ton);
-  tone(11, ton, 1000);
-  }
+	enc1.tick();    // оставляем тут для работы "временных" функций и антидребезга
 
- 
+	if (enc1.isRight())ton += 50;         // если был поворот
+	if (enc1.isLeft()) ton -= 50;
+	Serial.println(ton);
+	tone(11, ton, 1000);
+}
