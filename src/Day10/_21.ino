6004fc442d4258834e98d58c0af79d83d8d3946b
Sasha <lonagi22@gmail.com>
2020-06-29 18:27:34 +0300

    Update _21.ino

--- src/Day10/_21.ino
+++ src/Day10/_21.ino
@@ -19,54 +19,50 @@ DHT dht(DHTPIN, DHT11);
 RTC_DS1307 RTC;
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-void setup()
-{
-   lcd.init();                      // initialize the lcd 
-   lcd.backlight();
-   Wire.begin();
-   RTC.begin();
-   dht.begin();
-   RTC.adjust(DateTime(__DATE__, __TIME__));
+void setup(){
+  lcd.init();
+  lcd.backlight();
+  Wire.begin();
+  RTC.begin();
+  dht.begin();
+  RTC.adjust(DateTime(__DATE__, __TIME__));
 }
 
-void loop()
-{
- //Считываем влажность
+void loop(){
 
-float h = dht.readHumidity();
+  //Считываем влажность
+  float h = dht.readHumidity();
 
-// Считываем температуру
+  // Считываем температуру
+  float t = dht.readTemperature();
 
-float t = dht.readTemperature();
+  // Проверка удачно прошло ли считывание.
+  if (isnan(h) || isnan(t)) {
+    lcd.setCursor ( 11, 0 );
+    lcd.print ("EROR");
+  }
 
-// Проверка удачно прошло ли считывание.
-
-if (isnan(h) || isnan(t)) {lcd.setCursor ( 11, 0 );
-lcd.print ("EROR");
-  
-}
   lcd.clear();
-   DateTime now = RTC.now();
-lcd.setCursor ( 0, 0 );
+  DateTime now = RTC.now();
+  lcd.setCursor ( 0, 0 );
   lcd.print(now.hour(), DEC);
-    lcd.print(':');
-    lcd.print(now.minute(), DEC);
-    lcd.print(':');
-    lcd.print(now.second(), DEC);
-    lcd.setCursor ( 10, 0 );
-    lcd.print ("T:");
-    lcd.print (t);
-   lcd.setCursor(0, 1);
-   lcd.print(now.day(), DEC);
-    lcd.print('/');
-    lcd.print(now.month(), DEC);
-    lcd.print('/');
-    lcd.print(now.year(), DEC);
-    lcd.print(' ');
-    lcd.setCursor ( 10, 1);
-    lcd.print ("H:");
-    lcd.print (h);
-    
-    
+  lcd.print(':');
+  lcd.print(now.minute(), DEC);
+  lcd.print(':');
+  lcd.print(now.second(), DEC);
+  lcd.setCursor ( 10, 0 );
+  lcd.print ("T:");
+  lcd.print (t);
+  lcd.setCursor(0, 1);
+  lcd.print(now.day(), DEC);
+  lcd.print('/');
+  lcd.print(now.month(), DEC);
+  lcd.print('/');
+  lcd.print(now.year(), DEC);
+  lcd.print(' ');
+  lcd.setCursor ( 10, 1);
+  lcd.print ("H:");
+  lcd.print (h);
+
   delay(1000);
 }
